0fdc40d83e45a7a3b3fb61429ebf9425c16f880b
Cristine Shipman <cristine.shipman@gmail.com>
2017-07-18 14:45:48 -0500

    messing with nav bar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  });
+});
 
 
 // created list of writing prompts
